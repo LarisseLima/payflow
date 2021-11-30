81b1ea223c5479563d4852f6274e84171c57c55e
Larisse <larisse.lima2@gmail.com>
2021-11-30 20:24:16 -0300

    update app

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
-import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
-// ignore: use_key_in_widget_constructors
 class AppWidget extends StatelessWidget {
-  // ignore: use_key_in_widget_constructors
   AppWidget() {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitDown,
       DeviceOrientation.portraitUp,
     ]);
     SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(statusBarColor: AppColors.background));
+        const SystemUiOverlayStyle(statusBarColor: AppColors.primary));
   }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
       theme: ThemeData(
           primarySwatch: Colors.orange, primaryColor: AppColors.primary),
       initialRoute: "/splash",
@@ -29,6 +29,7 @@ class AppWidget extends StatelessWidget {
         "/home": (context) => const HomePage(),
         "/login": (context) => const LoginPage(),
         "/barcode_scanner": (context) => const BarcodeScannerPage(),
+        "/insert_boleto": (context) => const InsertBoletoPage()
       },
     );
   }
